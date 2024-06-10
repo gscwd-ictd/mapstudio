9779d17d2ac6e9f83a309609e1d70ee1e87704f7
Eric Sison <ericsison.dev@gmail.com>
2024-06-10 11:25:29 +0800

    added tailwind plugins and made custom tailwind config. moved styles folder to root

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,20 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    // app directory
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // components directory
+    "./src/lib/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/container-queries"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
 export default config;
