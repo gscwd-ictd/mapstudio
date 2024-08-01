b07e902c5724e39734d791b2d1a66cbd699339b9
Eric Sison <ericsison.dev@gmail.com>
2024-08-01 17:04:52 +0800

    fixed Calendar component

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,3 @@
-import { NextRequest, NextResponse } from "next/server";
-
-const protectedRoutes = ["/", "/dashboard"];
-
-export default async function middleware(request: NextRequest) {
-  return NextResponse.next();
-}
-
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
